1d6f9558053277799603e3a1136bebc1066d07a9
Luis_Munoz <dam@5300101A11117>
2018-01-09 17:07:33 +0100

    He añadido LibretaDirecciones.java

---
+++ src/controller/LibretaDirecciones.java
@@ -0,0 +1,55 @@
+package controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class LibretaDirecciones extends Application {
+
+    private Stage escenarioPrincipal;
+    private BorderPane layoutPrincipal;
+    private AnchorPane vistaPersona;
+
+    @Override
+    public void start(Stage escenarioPrincipal) {
+        this.escenarioPrincipal = escenarioPrincipal;
+        this.escenarioPrincipal.setTitle("Libreta de direcciones");
+        initLayoutPrincipal();
+        muestraVistaPersona();
+    }
+
+    private void initLayoutPrincipal() {
+        FXMLLoader loader = new FXMLLoader();
+        URL location = LibretaDirecciones.class.getResource("../view/VistaPrincipal.fxml");
+        loader.setLocation(location);
+        try {
+            layoutPrincipal=loader.load();
+        } catch (IOException ex) {
+            Logger.getLogger(LibretaDirecciones.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Scene escena = new Scene(layoutPrincipal);
+        escenarioPrincipal.setScene(escena);
+        escenarioPrincipal.show();
+        
+    }
+
+    private void muestraVistaPersona() {
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
